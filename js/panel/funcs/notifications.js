aca495029165708c11b9c0004c072a50131ea805
Ali82Aps <a.82aps@gmail.com>
2024-07-29 20:30:11 +0330

    Handle  notifications on Cms panel
    git push origin branch-name --force

---
+++ js/panel/funcs/notifications.js
@@ -0,0 +1,56 @@
+import { getToken } from "../../funcs/utils.js";
+
+const insertNotificationHtmlTemplate = (notifications) => {
+  const notificationModalListElem = document.querySelector(
+    ".home-notification-modal-list"
+  );
+
+  notificationModalListElem.innerHTML = ''
+
+  if (notifications.length) {
+    notifications.forEach((notification) => {
+      notificationModalListElem.insertAdjacentHTML(
+        "beforeend",
+        `
+          <li class="home-notification-modal-item">
+              <span class="home-notification-modal-text">${notification.msg}</span>
+              <a onclick='seenNotification(${JSON.stringify(notifications)}, ${JSON.stringify(notification._id)})'>دیدم</a>
+          </li>
+      `
+      );
+    });
+  } else {
+    notificationModalListElem.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li class="alert alert-danger text-center">
+          هیچ نوتیفیکیشنی وجود ندارد
+      </li>
+    `
+    );
+  }
+};
+
+const seenNotification = async (notifications, notificationID) => {
+  const res = await fetch(
+    `http://localhost:4000/v1/notifications/see${notificationID}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
+
+  removeNotification(notifications, notificationID)
+
+  const result = await res.json();
+};
+
+const removeNotification = (notifications, notificationID) => {
+  const filteredNotifications = notifications.filter(notification => notification._id !== notificationID)
+  
+  insertNotificationHtmlTemplate(filteredNotifications)
+}
+
+export { insertNotificationHtmlTemplate, seenNotification };
--- js/panel/shared.js
+++ js/panel/shared.js
@@ -1,19 +1,37 @@
 import { getAdminInfos } from "./funcs/utils.js";
+import { insertNotificationHtmlTemplate, seenNotification } from "./funcs/notifications.js";
 
-const $ = document
+window.seenNotification = seenNotification
 
-window.addEventListener('load', () => {
-    const adminWelcomeNameElem = $.querySelector('#admin-welcome-name')
-    const adminNameElem = $.querySelector('#admin-name')
-    getAdminInfos().then(admin => {
-        
-        // protect Cms routes
-        if (admin.role === 'ADMIN') {
-            // Show Admin Name In Cms Homepage
-            adminNameElem.innerHTML = admin.name
-            adminWelcomeNameElem.innerHTML = admin.name
-        }else {
-            location.replace('../../login.html')
-        }
+const $ = document;
+
+window.addEventListener("load", () => {
+  const adminWelcomeNameElem = $.querySelector("#admin-welcome-name");
+  const adminNameElem = $.querySelector("#admin-name");
+  const notificationsIconElem = $.querySelector('#notifications-icon')
+  const notificationsBoxElem = $.querySelector('.home-notification-modal')
+
+  getAdminInfos().then((admin) => {
+    console.log(admin);
+    // Protect Cms Routes
+    if (admin.role === "ADMIN") {
+      // Show Admin Name In Cms Homepage
+      adminNameElem.innerHTML = admin.name;
+      adminWelcomeNameElem.innerHTML = admin.name;
+    } else {
+      location.replace("../../login.html");
+    }
+
+    notificationsIconElem.addEventListener('mouseenter', () => {
+      notificationsBoxElem.classList.add('active-modal-notfication')
     })
-})
+
+    notificationsBoxElem.addEventListener('mouseleave', () => {
+      notificationsBoxElem.classList.remove('active-modal-notfication')
+    })
+    insertNotificationHtmlTemplate(admin.notifications)
+
+  });
+
+
+});
